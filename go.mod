ce2d7f2ee28a5b6558ea5b25aab63410476709fb
bjorndm <bjorn.de.meyer@gmail.com>
2025-02-20 23:34:44 +0900

    Start work on repository.

--- go.mod
+++ go.mod
@@ -4,5 +4,17 @@ go 1.22.0
 
 require (
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/klauspost/compress v1.17.11 // indirect
 	github.com/maxence-charriere/go-app/v10 v10.0.10 // indirect
+	github.com/minio/highwayhash v1.0.3 // indirect
+	github.com/nats-io/jwt/v2 v2.7.3 // indirect
+	github.com/nats-io/nats-server/v2 v2.10.25 // indirect
+	github.com/nats-io/nats.go v1.39.1 // indirect
+	github.com/nats-io/nkeys v0.4.9 // indirect
+	github.com/nats-io/nuid v1.0.1 // indirect
+	go.uber.org/automaxprocs v1.6.0 // indirect
+	golang.org/x/crypto v0.33.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
+	golang.org/x/time v0.9.0 // indirect
 )
--- pkg/repo/repo.go
+++ pkg/repo/repo.go
@@ -1,3 +1,111 @@
 // package repo is a repository where the data models can be stored to
 // and loaded from.
 package repo
+
+import "context"
+import "encoding/json"
+
+import nsrv "github.com/nats-io/nats-server/v2/server"
+import nats "github.com/nats-io/nats.go"
+import "github.com/nats-io/nats.go/jetstream"
+
+type Context = context.Context
+
+type Repository struct {
+	*nsrv.Server
+	*nats.Conn
+	jetstream.JetStream
+}
+
+func Open(sd string) (r *Repository, err error) {
+	opts := nsrv.Options{
+		JetStream: true,
+		StoreDir:  sd,
+	}
+	r = &Repository{}
+	r.Server, err = nsrv.NewServer(&opts)
+	if err != nil {
+		return nil, err
+	}
+	r.Conn, err = nats.Connect("", nats.InProcessServer(r.Server))
+	if err != nil {
+		return nil, err
+	}
+
+	r.JetStream, err = jetstream.New(r.Conn)
+	if err != nil {
+		return nil, err
+	}
+	return r, nil
+}
+
+func (r *Repository) Close() {
+	r.Conn.Close()
+	r.Server.Shutdown()
+}
+
+type BasicMapper[T any] struct {
+	Name string
+	*Repository
+	jetstream.KeyValue
+}
+
+func NewBasicMapper[T any](ctx Context, r *Repository, name string) (*BasicMapper[T], error) {
+	var err error
+
+	bm := &BasicMapper[T]{
+		Repository: r,
+		Name:       name,
+	}
+	bm.KeyValue, err = bm.Repository.JetStream.KeyValue(ctx, bm.Name)
+	if err != nil {
+		return nil, err
+	}
+	return bm, nil
+}
+
+func (b *BasicMapper[T]) Get(ctx Context, key string) (T, error) {
+	var obj T
+	entry, err := b.KeyValue.Get(ctx, key)
+	if err != nil {
+		return obj, err
+	}
+
+	err = json.Unmarshal(entry.Value(), &obj)
+	if err != nil {
+		return obj, err
+	}
+
+	return obj, nil
+}
+
+func (b *BasicMapper[T]) Put(ctx Context, key string, obj T) (T, error) {
+	var zero T
+
+	buf, err := json.Marshal(obj)
+	if err != nil {
+		return zero, err
+	}
+
+	_, err = b.KeyValue.Put(ctx, key, buf)
+	if err != nil {
+		return zero, err
+	}
+	return obj, nil
+}
+
+func (b *BasicMapper[T]) Purge(ctx Context, key string) error {
+	return nil
+}
+
+func (b *BasicMapper[T]) Watch(ctx Context, pattern string) (chan (T), error) {
+	return nil, nil
+}
+
+func (b *BasicMapper[T]) All(ctx Context, pattern string) (chan (T), error) {
+	return nil, nil
+}
+
+func (b *BasicMapper[T]) Keys(ctx Context, pattern string) (chan (string), error) {
+	return nil, nil
+}
